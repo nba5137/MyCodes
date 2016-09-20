ae07f9f9dc4aa95d8ca08ad54e4d8a74f2829591
Sibo Song <siso8575@colorado.edu>
2016-09-20 13:44:06 -0600

    A program to calculate the average. For recalling.

---
+++ average.cpp
@@ -0,0 +1,45 @@
+// A program that calculate the average of numbers which a user input
+// Purpose is to recall shit in my mind
+
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void ave()
+{
+	int x, y, n, nw;
+	cout << "Input the number of numbers you want to input:";
+	cin >> n;
+	if (n < 1)
+	{
+		cout << "That's shit." << endl;
+	} 
+	else
+	{
+		nw = n;
+	}
+	while (n != 0)
+	{
+		cin >> x;
+		if (x < 0)
+		{
+			cout << "That's shit." << endl;
+		} 
+		else
+		{
+			y = y + x;
+			n--;
+		}
+	}
+	int avg;
+	avg = y / nw;
+	cout << "Your shit is: " << avg << endl;
+}
+
+int main()
+{
+	ave();
+	return 0;
+}
