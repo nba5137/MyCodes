4218b100653aff16301121c150d7bb880a6c7738
Sibo Song <siso8575@colorado.edu>
2016-09-20 13:07:54 -0600

    A question from Baidu about how to print out "ab".

---
+++ printab.cpp
@@ -0,0 +1,60 @@
+// The c++ solution for a question from Baidu
+/*
+ * How to print out "ab":
+ * 
+ * public void print()
+ * {
+ * 		if(		)
+ * 		{
+ * 			System.out.print("a");
+ * 		}
+ * 		else
+ * 		{
+ * 			System.out.print("b");
+ * 		}
+ * }
+ * 
+ * ------------------------------------------- Java 8(with lambda) By: RednaxelaFX ------------------------------------
+ * 
+ * import java.util.function.BooleanSupplier;
+ * 
+ * public class xx
+ * {
+ * 		public static void main(String[]args)
+ * 		{
+ * 			if ( ((BooleanSupplier)( () -> {System.out.print("a"); return false;} )).getAsBoolean() )
+ * 			{
+ * 				System.out.print("a");
+ * 			}
+ * 			else
+ * 			{
+ * 				System.out.print("b");
+ * 			}
+ * 		}
+ * }
+ * 
+ * ------------------------------------------- Java 8(with lambda) By: RednaxelaFX ------------------------------------
+ *  
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void print()
+{
+	if (cout << "a", false) //print out "a" and return false
+	{
+		cout << "a";  //leap 
+	}
+	else //which means when false
+	{
+		cout << "b" ; //print out "b"
+	}
+}
+
+int main()
+{
+	print();
+	return 0;
+}
